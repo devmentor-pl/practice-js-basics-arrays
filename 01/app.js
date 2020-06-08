827ed63607fb2d38556258e9fddda86c494d73b3
Martyna Kiljan <kmartynak@icloud.com>
2020-06-08 15:19:55 +0200

    01_tablice

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+const users = ['Ania Kowalska', 'Wojtek Blachowski', 'Władysław Urbański', 'Ola Smołek', 'Kasia Stępień'];
+
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
+console.log(users.length);
