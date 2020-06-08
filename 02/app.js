4fe1ccdbaa87ec90cc1a95e5fd3b9048c93bd470
Martyna Kiljan <kmartynak@icloud.com>
2020-06-08 15:53:00 +0200

    02_tablice

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,15 @@
 const randomArray = createRandomArray();
-console.log(randomArray);
+// console.log(randomArray);
+
+for (let i = 0; i < randomArray.length; i++) {
+    console.log('Wylosowana liczba to:' + ' ' + randomArray[i]);
+}
+
+randomArray.forEach(num => console.log(num));
+
+
+let lastIndex = randomArray.length - 1;
+console.log('Ostatnia wylosowana liczba to:' + ' ' + (randomArray[lastIndex]));
 
 
 
@@ -11,13 +21,13 @@ console.log(randomArray);
 function createRandomArray() {
     const arr = [];
     const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
+    for (let i = 0; i < len; i++) {
+        arr.push(getRandomInteger(1, 100));
     }
 
     return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
