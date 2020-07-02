5f9879d4ed5de834b53bc567df7136e09850ad9d
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-02 18:59:43 +0200

    task 4 needs help

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
+const today = new Date().getFullYear();
+const diff = years.map(getDiff)
+
+function getDiff() {
+    for (let i = 0; i < years.length; i++) {
+        const result = today - years[i];
+        console.log(result)
+    }
+
+};
+console.log(diff)
