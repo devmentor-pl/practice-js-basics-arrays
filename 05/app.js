5a53e0dba73313c9d922d4b900921c2e3a097139
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-02 22:17:39 +0200

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,17 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(num) {
+    return num % 2 === 0;
+})
+console.log(evenNumbers)
+
+
+const getSum = arr => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+const sum = getSum(evenNumbers);
+console.log(sum)
