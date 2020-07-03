a57459628329c6ab4ce4270c91cd1ada55310991
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-03 09:37:32 +0200

    add reduce method

--- 05/app.js
+++ 05/app.js
@@ -7,11 +7,13 @@ console.log(evenNumbers)
 
 
 const getSum = arr => {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i]
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i]
+        }
+        return sum
     }
-    return sum
-}
-const sum = getSum(evenNumbers);
+    // const sum = getSum(evenNumbers);
+    // console.log(sum)
+const sum = evenNumbers.reduce((x, y) => x + y)
 console.log(sum)
