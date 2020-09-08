35ff542e649392606ccb7aa2fb7b954cef262c61
AnnaBuller <aniabela@wp.pl>
2020-09-08 12:21:13 +0200

    solve 01 task

---
+++ 01/app.js
@@ -0,0 +1,3 @@
+const users = ["John Doe", "Jenny Doe", "Bob Marley", "Benny Dyle", "Miriam Lopez"];
+
+console.log(`We have ${users.length} users. Thee of them are: ` + users[0] + ", " + users[2] + ", " + users[4] + ".")
