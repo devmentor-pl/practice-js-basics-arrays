4cff66ffce9fef18dca3b39160efb6fd35fadd5a
AnnaBuller <aniabela@wp.pl>
2020-09-08 18:20:44 +0200

    solve 05 task

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,20 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenTotal = sumEven(numbers);
+
+function sumEven(arr) {
+  return arr.filter(isEven).reduce(function(a, b) {return a + b})
+}
+
+function isEven(num) {
+  return num % 2 === 0
+}
+
+console.log(evenTotal)
+
+// Teoretycznie można powyższe skrócić jeszcze bardziej, tylko tracimy możliwość wykorzystania funkcji isEven gdzieś indziej (zmniejsza się też czytelność :P):
+
+function sumEven(arr) {
+  return arr.filter(function(num) {return num % 2 === 0}).reduce(function(a, b) {return a + b})
+}
+
+console.log(evenTotal)
