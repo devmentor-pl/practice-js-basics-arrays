78c0d0d524a3c90661ac89b55ac7d28005410713
AnnaBuller <aniabela@wp.pl>
2020-09-08 13:16:16 +0200

    solve 03 task

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,20 @@
 const n = 24;
 const oddNumbers = [];
+console.log(findOddNums(n))
+
+function findOddNums(n) {
+  let arr = [];
+  if (n === 0) {
+    console.log('0 is neither an odd nor an even number')
+  }
+  for (let i = 1; i <= n; i++) {
+    if (ifOdd(i)) {
+      arr.push(i)
+    }
+  }
+  return arr
+}
+
+function ifOdd(num) {
+  return num % 2 === 1
+}
