fc6758e83c142a07dc0162e4387ba427133fc9b4
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-12 10:52:01 +0200

    Create filter and reduce methods - Task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,12 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbersSum = numbers.filter(onlyEven).reduce(sum);
+console.log(evenNumbersSum);
+
+
+function onlyEven(element){
+    return element%2===0;
+}
+
+function sum(a,b){
+    return a+b;
+}
