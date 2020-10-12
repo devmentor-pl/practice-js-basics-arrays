a34ee5179fef1ac5c56d39face03c5e6353e19ef
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-12 10:47:05 +0200

    Create map method - Task 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
+const yearsFromNow = years.map(calcYears);
+console.log(yearsFromNow);
+
+
+function calcYears(element){
+    const nowYear = new Date().getFullYear();
+    return nowYear-element;
+}
