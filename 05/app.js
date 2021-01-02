74d728bf40100b0b1f0cbfad4042a540ff1c7ca2
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-02 21:21:02 +0100

    Zad 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,19 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const even = numbers.filter(isEven);
+console.log(even);
+
+// const even2 = numbers.filter(function(number) {
+//     return number % 2 === 0;
+// })
+// console.log(even2);  
+const sum = even.reduce(getSum);
+console.log(sum);
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+function getSum(total, num) {
+    return total + num;
+}
