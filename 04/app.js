2b69afe637b4f4aac9d30be732fdd7308131ad5c
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-02 20:30:03 +0100

    Zad 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,6 @@
 const years = [1980, 1934, 2002, 2019];
+const difference = years.map(function(oldYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - oldYear;
+})
+console.log(difference);
