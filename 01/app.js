3e21a674ce8eb96433fa6be44368e6bbd67fdaac
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-02 03:55:19 +0100

    Zad 01

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+
+const users = ["Anna Kołodziej", "Ewa Skórska", "Marek Mitko", "Mateusz Bogolubow", "Hanna Król", "Julia Korabiewska"];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
+console.log(users.length);
