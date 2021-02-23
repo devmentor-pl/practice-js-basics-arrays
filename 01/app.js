e31494de3a3251895f1bd09ce9655b2dd269fbc1
Agnieszka Polińska <agapoli@gmail.com>
2021-02-23 16:52:29 +0100

    Add array and console.log info about it

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+const users = ['Jan Nowak', 'Adam Kowalski', 'Anna Kubiak', 'Agnieszka Kwiatkowska', 'Magdalena Walczak'];
+
+console.log(`${users[0]}, ${users[2]}, ${users[4]}`);
+console.log(`Tablica posiada ${users.length} elementów.`);
+
