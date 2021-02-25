4664519a8303a3540eb5d00802edd5cc7990d2ea
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-02-25 22:41:25 +0000

    Wszystkie zadania wykonane

---
+++ 01/app.js
@@ -0,0 +1,12 @@
+const users = [
+  'Jan Kowalski',
+  'Adam Nowak',
+  'John Doe',
+  'Grzegorz Brzęczyszczykiewicz',
+  'Sebastian Matysiak',
+];
+
+console.log(`Pierwszy użytkownik to: ${users[0]}`);
+console.log(`Trzeci użytkownik to: ${users[2]}`);
+console.log(`Piąty użytkownik to: ${users[4]}`);
+console.log(`Tablica users zawiera ${users.length} użytkowników`);
