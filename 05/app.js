611ae875d6ff684fc3911f5bd02facc57049fc4f
Agnieszka Polińska <agapoli@gmail.com>
2021-03-01 19:14:52 +0100

    Refactor code

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const sumOfEvenNumbers = numbers.filter(number => number%2 === 0).reduce((prev, curr) => prev + curr);
+const evenNumbers = numbers.filter(number => number%2 === 0);
+const sumOfEvenNumbers = evenNumbers.reduce((prev, curr) => prev + curr, 0);
 console.log(sumOfEvenNumbers);
