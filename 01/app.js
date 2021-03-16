8e32a16a908bb358eeec29b934d9837f116cd6a0
GrzegorzGozdziak <ggozdziak@gmail.com>
2021-03-16 22:55:10 +0100

    answers

---
+++ 01/app.js
@@ -0,0 +1,3 @@
+let users = [ 'John Doe', 'Jan Kowal', 'Ada Nowak', 'Miko Lars', 'Zoe Lama', 'Ewa Adam' ];
+
+console.log(users[0], users[2], users[4], users.length);
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,6 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+const numbers = [ 1, 2, 3, 4, 5, 6, 7 ];
+
+let z = numbers.filter((i) => i % 2 === 0).reduce((x, y) => {
+	return x + y;
+}, 0);
+console.log(z);
