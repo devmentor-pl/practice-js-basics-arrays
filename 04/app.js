efa1c2ec76e49a78b15eab657a3887228ef216df
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-18 16:09:21 +0200

    Task 04: years
    solutions without and with .map()

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,20 @@
 const years = [1980, 1934, 2002, 2019];
+let yearsDifference;
+let yearsDifferences = [];
+let today = new Date();
+let currentYear = today.getFullYear();
+
+//solution without .map()---------------------------------------------------------------
+
+for (let i = 0; i < years.length; i++) {
+    yearsDifference = currentYear - years[i];
+    yearsDifferences.push(yearsDifference);
+}
+
+console.log('Solution without .map(): ' + yearsDifferences)
+
+//solution with .map()------------------------------------------------------------------
+
+let yearsDifferencesWithMap = years.map(function(year) {return currentYear - year;});
+
+console.log('Solution with .map(): ' + yearsDifferencesWithMap)
