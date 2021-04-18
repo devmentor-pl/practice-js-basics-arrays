0d6b665297ce973c94b568759954bc011cc98548
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-18 13:44:59 +0200

    Task 03: oddNumbers
    Task 03: oddNumbers

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,22 @@
-const n = 24;
+const n = 100;
 const oddNumbers = [];
+
+//first solution
+
+for (let i = 1; i <= n; i = i + 2) {
+    oddNumbers.push(i);
+}
+
+console.log('first solution: ' + oddNumbers)
+
+//second solution
+
+const oddNumbers2 = [];
+
+for (let i = 0; i <= n; i++) {
+    if (i % 2 == 1) {
+        oddNumbers2.push(i);
+    }
+}
+
+console.log('second solution: ' + oddNumbers2)
