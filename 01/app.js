d16600abd8457807caff5c0843e994755c746ea1
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-24 21:30:58 +0200

    Finish task 01

---
+++ 01/app.js
@@ -0,0 +1,3 @@
+const users = ['Gosia Kowacka', 'Jan Nowak', 'Tomasz Wilk', 'Ala T.', 'Adam Kot'];
+console.log(users[0], users[2], users[4]);
+console.log('Ilość elementów tablicy to:', users.length);
