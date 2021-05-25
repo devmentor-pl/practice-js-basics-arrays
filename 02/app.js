361d57673c20c52ec3fb77c98725921a2877c241
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-25 07:13:24 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,17 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
 
+for (let i = 0; i < randomArray.length; i++) {
+    console.log(randomArray[i]);
+}
 
+randomArray.forEach(showItems);
+
+function showItems(item) {
+    console.log(item);
+}
+
+console.log('Ostatni element tablicy to: ' + randomArray[randomArray.length - 1]);
 
 // nie modyfikuj kodu poniżej!
 
@@ -11,13 +21,13 @@ console.log(randomArray);
 function createRandomArray() {
     const arr = [];
     const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
+    for (let i = 0; i < len; i++) {
+        arr.push(getRandomInteger(1, 100));
     }
 
     return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
