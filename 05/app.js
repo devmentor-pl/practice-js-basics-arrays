f2fefe03ff5179b6854594e11343ff6ca84a49e6
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-25 08:24:45 +0200

    Finish task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = numbers.filter(function (number) {
+    if (number % 2 === 0) {
+        return number;
+    }
+});
+
+console.log(evenNumbers);
+
+const sum = evenNumbers.reduce(function (previousNum, currentNum) {
+    return previousNum + currentNum;
+});
+
+console.log(sum);
