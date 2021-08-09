1790068e5685be116ab770124357289095e0689a
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-09 11:05:06 +0200

    Finish task 01

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const users = [
+  "John Doe",
+  "Eric Barber",
+  "Geneva Henry",
+  "Jennifer Moore",
+  "Isabel Sanders",
+  "Grace Stevens",
+  "Seth Garza",
+];
+
+console.log(users[2]);
+console.log(users[4]);
+console.log(users.length);
