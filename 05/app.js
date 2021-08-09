27fc604acb3456cd798f66da90a36ff4d99fd14c
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-09 12:01:58 +0200

    Finish task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function (el) {
+  return el % 2 == 0 ? true : false;
+});
+
+const sum = evenNumbers.reduce(function (total, el) {
+  return total + el;
+});
+
+console.log(numbers);
+console.log(evenNumbers);
+console.log(sum);
