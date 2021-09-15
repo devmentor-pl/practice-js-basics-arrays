152a18bdddd208f091c9513362ac50fc2b265386
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-15 14:10:42 +0200

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,19 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
 
+// A
+for (let i = 0; i < randomArray.length; i++) {
+	console.log(randomArray[i]);
+}
 
+console.log('BREAK');
+
+// B
+randomArray.forEach((el) => console.log(el));
+
+// C
+const lastArrayElement = randomArray[randomArray.length - 1];
+console.log(lastArrayElement);
 
 // nie modyfikuj kodu poniżej!
 
@@ -9,15 +21,15 @@ console.log(randomArray);
 // ponieważ w JS występuje mechanizm tzw. hoisting-u
 
 function createRandomArray() {
-    const arr = [];
-    const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
-    }
+	const arr = [];
+	const len = getRandomInteger(1, 10);
+	for (let i = 0; i < len; i++) {
+		arr.push(getRandomInteger(1, 100));
+	}
 
-    return arr;
+	return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+	return Math.round(Math.random() * (max - min) + min);
 }
