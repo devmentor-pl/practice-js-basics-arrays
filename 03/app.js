5ea612966978fa2738c94090e02dc59ebf266f90
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-15 14:19:15 +0200

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,13 @@
 const n = 24;
 const oddNumbers = [];
+
+function oddNumbersSeries(n) {
+	for (let i = 1; i < n; i++) {
+		if (i % 2 !== 0) {
+			oddNumbers.push(i);
+		}
+	}
+	return oddNumbers;
+}
+
+console.log(oddNumbersSeries(n));
