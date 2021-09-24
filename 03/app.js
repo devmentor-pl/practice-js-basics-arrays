33efea75413e516018259a06f263110c78256d4b
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-24 22:17:00 +0200

    Third task

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,15 @@
 const n = 24;
 const oddNumbers = [];
+
+
+for (i=1; i<=n; i++) {  // i=1 bo od 1 do n 
+    // console.log(i);
+
+    if (i % 2 === 1) {
+        oddNumbers.push(i);
+    }
+}
+
+console.log(oddNumbers);
+
+//powinienem uzyc tutaj funkcji czy jest w porzadku?
