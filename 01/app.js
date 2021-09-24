15eaaa2bd1dc9a4c7733f0c1f5f34d122bd4446e
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-24 22:03:32 +0200

    First task

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+const users = [];
+
+users.push("Andrzej Kowalski", "Tomasz Grab", "Jan Nowak", "Piotr Żyła", "Marcin Najman");
+
+console.log(users);
+
+console.log(users.length);
+
+console.log(users[0], users[2], users[users.length-1]);
