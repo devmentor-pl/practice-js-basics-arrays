216b823bd6702b87a51043e77f3012ffa3e5bc05
Rafał Kazik <rafalkazik@MacBook-Air-Rafal.local>
2021-10-30 01:24:26 +0200

    arrays task finished

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,7 @@
 const years = [1980, 1934, 2002, 2019];
+
+const countYears = years.map(function (year) {
+  return 2021 - year;
+});
+
+console.log(countYears);
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function (takeEven) {
+  return takeEven % 2 === 0;
+});
+
+const sumEven = evenNumbers.reduce(function (even, index) {
+  return even + index;
+});
+
+console.log(sumEven);
