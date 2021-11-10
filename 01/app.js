4fba321baaf08619aab9fdd87f2fedf13300559e
Ewelina <ewelina@ekopacz.pl>
2021-11-10 22:47:37 +0100

    add code

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+const user = ['Jan Zyto','Kamil Rzepak','Julka Owska','Kamila Jularka','Bogdan Szomek','Dominika Szumacher'];
+
+console.log('Pierwszy element to: '+ user[0]);
+console.log('Pierwszy element to: '+ user[2]);
+console.log('Pierwszy element to: '+ user[4]);
+console.log('Ilosc elementów w tablicy to: ' + user.length);
+
+console.log('---------------- Drugi sposób liczby nieparzyste ----------------')
+
