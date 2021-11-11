8ebc64feb185f9420a827d006a1f5029540bde53
Ewelina <ewelina@ekopacz.pl>
2021-11-11 22:19:44 +0100

    upgrade

--- 03/app.js
+++ 03/app.js
@@ -3,8 +3,6 @@ const oddNumbers = [];
 const resultArray = addCheckNumbers(oddNumbers);
 console.log(resultArray);
 
-
-
 function addCheckNumbers(array){
     for(let i=1;i<=n; i++){
         if(i % 2 !== 0){
