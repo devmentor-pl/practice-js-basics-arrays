e9c302ee45743092a6cb5e975620e8b2e699b5f8
Ewelina <ewelina@ekopacz.pl>
2021-11-11 21:40:16 +0100

    add array

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,16 @@
 const n = 24;
 const oddNumbers = [];
+const resultArray = addCheckNumbers(oddNumbers);
+console.log(resultArray);
+
+
+
+function addCheckNumbers(array){
+    for(let i=1;i<=n; i++){
+        if(i % 2 !== 0){
+            array.push(i);
+        }
+    }return array;
+}
+
+
