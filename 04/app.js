bf4f7b9374ebcb4cb2a14ed87f31c0eac919a518
Ewelina <ewelina@ekopacz.pl>
2021-11-11 21:52:21 +0100

    add map

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+const yearsResult = years.map(checkYears);
+console.log("Start tablica: " + years);
+console.log("Nowa tablica: " + yearsResult);
+
+function checkYears(element, index){
+    return element = 2021 - element;
+}
