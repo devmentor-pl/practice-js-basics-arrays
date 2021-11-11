dd820d6c466f71b4545e13b95b2cd936ac87e238
Ewelina <ewelina@ekopacz.pl>
2021-11-11 22:16:49 +0100

    add functions

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const result = numbers.filter(isEven);
+const resultSum = result.reduce(addSum);
+console.log("Suma wszystkich liczb parzystych z tablicy: " + resultSum);
+
+function isEven(value){
+    return value % 2 === 0;
+}
+
+function addSum(total, value){
+    return total + value;
+}
