2f07ac5addf1b2d48dba41f032fd997fa482e8e9
Max <maciejdobisz@gmail.com>
2022-01-14 10:56:18 +0000

    practice arrays

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,8 @@
-const n = 24;
+const n =24;
 const oddNumbers = [];
+
+while(oddNumbers.length<n){
+    randomNumber=Math.round(Math.random()*100);
+    if(randomNumber%2!==0&oddNumbers.length<=n) {
+    oddNumbers.push(randomNumber)};
+}
--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,5 @@
 const years = [1980, 1934, 2002, 2019];
+
+const yearsUpToday=years.map(function(element){
+    return 2022-element;
+});
