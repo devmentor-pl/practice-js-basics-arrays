5199edd5c5a9c031f4ece454df1052054022188c
Anna <krolikowska.ann@gmail.com>
2022-02-10 17:42:56 +0100

    02 added randomArray elements

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,17 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
 
+//  for(let i=0; i<randomArray.length; i++) {
+//     console.log(randomArray[i]);
+//  }
+
+randomArray.forEach(function(element, index, array) {
+    console.log(element);
+});
+
+const lastElement = randomArray[randomArray.length - 1];
+console.log(lastElement)
+
 
 
 // nie modyfikuj kodu poniżej!
