8960bb99ac3b19f42d29451be819fba7ee5d6c81
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-02-14 22:28:16 +0100

    Add solutions to exercise 03, 04 and 05

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,10 @@
 const years = [1980, 1934, 2002, 2019];
+
+const subtractionResult = years.map(subtraction);
+
+console.log(subtractionResult);
+
+function subtraction(element) {
+    return 2022 - element;
+}
+
