ee2104e392d9195ddd96d76d0b9d47f9ab0924aa
Jakub Parol <kubciop33@gmail.com>
2022-03-01 15:01:42 +0100

    arrow practice

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+const yearsToToday = years.map(toToday);
+
+console.log(yearsToToday);
+
+function toToday(element) {
+    return 2022 - element;
+}
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = numbers.filter(isEven);
+const sumEvenNumbers = evenNumbers.reduce(sum);
+
+function isEven(element) {
+    return element % 2 === 0;
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sumEvenNumbers);
