068c74ed97d650c25616d20dba173b17000ffef7
Piotr <piotr.wojcikievvicz@gmail.com>
2022-03-06 08:13:20 +0100

    completed

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(element){ if (!(element%2)) return element });
+const sumEvenNum = evenNumbers.reduce(getSum,0);
+console.log(evenNumbers);
+console.log(sumEvenNum);
+
+
+function getSum(total, numbers){
+    return total + numbers;
+}
