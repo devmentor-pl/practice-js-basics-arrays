5b6c452d93c6c891aabfee1a9449ec0ca8592df4
Lukasz Konatowski <konat1993@gmail.com>
2022-03-11 22:20:21 +0100

    task #3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,8 @@
-const n = 24;
-const oddNumbers = [];
+const n = 24
+const oddNumbers = []
+
+for (let i = 1; i <= n; i++) {
+    i % 2 !== 0 ? oddNumbers.push(i) : null
+}
+
+console.log({ oddNumbers })
