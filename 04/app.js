f137e34c5e1a72e788447ecd6b25f09d96ceaed6
Martyna Giee <martyna.giee@gmail.com>
2022-03-22 19:12:55 +0100

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,13 @@
 const years = [1980, 1934, 2002, 2019];
+
+let countYearsFromNow = (array) => {
+
+    const year = new Date().getFullYear();
+    const yearsFromNow = array.map(x => year - x);
+
+    array.forEach(function (element, index) {
+        console.log(`Od ${element} do ${year} minęło ${yearsFromNow[index]} lat`);
+    })
+};
+
+countYearsFromNow(years);
