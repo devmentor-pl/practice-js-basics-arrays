e04c1d03356a67c676a101409455d529520e361a
Filip <43039909+Urfisior@users.noreply.github.com>
2022-03-23 21:04:23 +0000

    task 05 done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenSumNumbers = numbers.filter(function (el) {
+    return !(el % 2);
+}).reduce(function (acc, el) {
+    return acc + el;
+});
+
+console.log(evenSumNumbers);
