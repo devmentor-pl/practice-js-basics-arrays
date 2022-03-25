8fc65d896d6f12d3049b05c3e4e80ba66a76cb16
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-03-25 12:27:41 +0100

    practice arrays

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+const users = ['Tom Stanley', 'Wayne Webb', 'Julia Barnett', 'Grace Lewis', 'Ivan Turner', 'Everett Fisher'];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log(users.length);
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+
+function evenNumbers(number){
+    return number % 2 === 0;
+}
+
+const numbers2 = numbers.filter(evenNumbers);
+
+const numbers3 = numbers2.reduce(function(a, b) {
+    return a + b;
+})
+
+console.log(numbers3);
