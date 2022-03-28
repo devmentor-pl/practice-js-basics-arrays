87321fe15f3b894f67026bec406649dcb1f758fd
Andrzej <andis_as@wp.pl>
2022-03-28 18:54:40 +0200

    zad 1 - done

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+
+const users = ['Ala Alkowska', 'Ola Olkowska', 'Iza Izakowa', 'Hela Helkowska', 'Fele Felkowska']
+
+console.log((0+1)+')', users[0])
+console.log((2+1)+')', users[2])
+console.log((4+1)+')', users[4])
+
+console.log('All users: ', users.length)
+
+
+
+
+
+
+
+
