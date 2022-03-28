82b842c8651b6f262f8969645091f7eae059f250
Andrzej <andis_as@wp.pl>
2022-03-28 20:26:59 +0200

    zad 4 - done

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,26 @@
-const years = [1980, 1934, 2002, 2019];
+const years = [1980, 1934, 2002, 2019]
+
+
+// Actual year
+const currentYear = (new Date()).getFullYear()
+console.log('current year: ' + currentYear)
+
+
+// anonymous function as as param
+const yearsPassed = years.map(function(year) {
+    const currentYear = (new Date()).getFullYear()
+    return currentYear - year
+})
+console.log(yearsPassed)
+
+
+// named function as param
+function newYears(year) {
+    const currentYear = (new Date()).getFullYear()
+    return currentYear - year
+}
+
+const yearsPassed2 = years.map(newYears)
+console.log(yearsPassed2)
+
+
