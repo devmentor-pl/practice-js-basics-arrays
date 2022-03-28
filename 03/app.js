f4e7e4b9705b531c789a9b7f1cad9cbd7d3ab789
Andrzej <andis_as@wp.pl>
2022-03-28 20:02:40 +0200

    zad 3 - done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,19 @@
 const n = 24;
 const oddNumbers = [];
+
+let i = 0
+while (i < n) {
+    const randomNumber = Math.round(Math.random() * 100)
+    if (randomNumber % 2 !== 0) {
+        console.log(i, randomNumber, 'okey added')
+        oddNumbers.push(randomNumber)
+        i++
+    } else {
+        console.log(i, randomNumber, 'fail')
+    }
+}
+
+console.log(oddNumbers)
+
+
+
