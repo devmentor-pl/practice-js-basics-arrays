793508853e7a32869225eceb932a9e366b4024e0
Mateusz Suplewski <matx3582@gmail.com>
2022-03-30 13:36:52 +0200

    js arrays done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,16 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+
+const evenNumbers = numbers.filter(getEvenNumbers);
+
+const result = evenNumbers.reduce(addElements);
+
+console.log(result);
+
+function getEvenNumbers(number) {
+    return !(number % 2);
+}
+
+function addElements(total, num) {
+    return total + num;
+}
