12961dac325590b0409684a314bcd72d6668f97c
MaciejKasperowicz <makasp@wp.pl>
2022-04-01 12:38:30 +0200

    Task 4 completed

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+const thisYear = new Date().getFullYear();
+
+const yearsGone = years.map(function(elem){
+    return (thisYear - elem);
+});
+
+console.log(yearsGone);
