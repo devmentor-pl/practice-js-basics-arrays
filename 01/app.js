06a60d79abe374713c00afea868260336f67e990
Dawid Drag <dede7d@wp.pl>
2022-04-04 18:12:47 +0200

    added js code

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+const users = new Array('Jan Kowalski 0', 'Jan Kowalski 1', 'Jan Kowalski 2', 'Jan Kowalski 3', 'Jan Kowalski 4', 'Jan Kowalski 5');
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log('liczba elementów w tablicy ' + users.length);
