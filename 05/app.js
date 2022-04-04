b6e1a1751f52b2ff7abdd2c330a776c833d3ab70
Lukasz Scisel <lukscidev@gmail.com>
2022-04-04 20:10:59 +0200

    05 done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,9 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const filteredNumbers = numbers.filter(element => element % 2 === 0)
+
+console.log(filteredNumbers)
+
+const sumOfFilteredNumbers = filteredNumbers.reduce((a, b) => a + b);
+
+console.log(sumOfFilteredNumbers)
