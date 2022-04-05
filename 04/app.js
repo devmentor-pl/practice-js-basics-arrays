5a7cf41f7e40d6b932c46a8d38ae945e735a13e9
Dawid Drag <dede7d@wp.pl>
2022-04-05 17:49:22 +0200

    changed js code

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,5 @@
 const years = [1980, 1934, 2002, 2019];
 
-years.forEach(function (element) {
-    console.log(element);
-});
-
 
 function newYears(element) {
     const timeBetweenYears = new Date().getFullYear() - element;
