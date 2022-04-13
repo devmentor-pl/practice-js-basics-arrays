1a7154e8486331c23f298630b9b14718df908740
Joanna Markiewicz <jmarkiewicz1@op.pl>
2022-04-13 21:21:10 +0200

    05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,17 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(getEvenNumbers);
+
+function getEvenNumbers(element) {
+    return element % 2 === 0;
+};
+
+console.log(evenNumbers);
+
+const sum = evenNumbers.reduce(getSum);
+
+function getSum(total, num) {
+    return total + num;
+}
+
+console.log(sum)
