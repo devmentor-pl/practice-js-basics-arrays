ea82bdc80f9283fa5f45061f52d4654037be8baa
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-02 16:10:33 +0200

    Done 04 task

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+const newYear = (new Date().getFullYear());
+
+const yearsPassed = years.map((el) => {
+    return newYear - el;
+})
+
+console.log(yearsPassed);
