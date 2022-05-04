e5161ccd7e7a839b6991e3f12f865e824ed8db88
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-04 16:32:53 +0200

    fix 05 task

--- 05/app.js
+++ 05/app.js
@@ -1,10 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
-const evenNumbers = numbers.filter( (number) => {
-	if (number % 2 === 0) {
-		return number;
-	} else {
-		return false;
-	}
+const evenNumbers = numbers.filter(number => {
+	return number % 2 === 0;
 });
 
 const sum = evenNumbers.reduce(function (a, b) {
