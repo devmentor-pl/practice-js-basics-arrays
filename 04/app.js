4117703ef5e00a5bba8f4fa518c502d5a4f6cd11
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-05-19 20:26:02 +0200

    end task 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
+
+const now = 2022;
+
+const difference = years.map(differenceFunction);
+
+function differenceFunction(element) {
+    return now - element
+}
+
+
+console.log(difference)
