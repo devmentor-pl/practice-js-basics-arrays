bfa17a92ed9a7effcb56d223ee2746722919a88d
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-05-19 18:58:01 +0200

    end task 02

--- 01/app.js
+++ 01/app.js
@@ -2,6 +2,6 @@
 
 const users =['Ola Nowak', 'Michał Nowy', 'Piotr Jelcz', 'Pola Rola', 'Tomasz Wnęk' ];
 
-console.log(users[0] +   users[2]   + users[4]);
+console.log(users[0] +  users[2]    +  users[4]);
 
 alert(users.length)
