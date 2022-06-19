e2a00eb8ab373168bb70e13abf5466f7e465cdec
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-19 18:12:05 +0200

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,9 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenSumNumbers = numbers
+	.filter(function (e) {
+		return !(e % 2);
+	})
+	.reduce(function (sum, e) {
+		return sum + e;
+	});
+console.log(evenSumNumbers);
