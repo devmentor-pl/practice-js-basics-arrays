858c6d5e7e6ce949b9e35609ed195ac374e7ca85
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-08-06 03:03:39 +0200

    finish task arrays02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,16 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
 
+for(let i=0; i<randomArray.length; i++) {
+    console.log(randomArray[i]);
+}
 
+randomArray.forEach(function(item) {
+    console.log(item);
+});
+
+const lastElement = randomArray.length - 1;
+console.log(randomArray[lastElement])
 
 // nie modyfikuj kodu poniżej!
 
