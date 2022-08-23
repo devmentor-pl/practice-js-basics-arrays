2b1c257d0bbafaf5fedf428ab47143bed0ae6f08
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-08-23 13:58:13 +0200

    Change functo arrow func

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,4 @@
 const years = [1980, 1934, 2002, 2019];
 
-const passYears = years.map(function(element){ return 2022 - element});
+const passYears = years.map(element => 2022 - element);
 console.log(passYears);
