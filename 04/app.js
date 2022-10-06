89933bf2aec951cd567358153dd5cdebeea34fa6
Tomek2112 <kaminski_tomasz@interia.pl>
2022-10-06 13:12:24 +0200

    done

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,14 @@
-const years = [1980, 1934, 2002, 2019];
+ const years = [1980, 1934, 2002, 2019];
+const modifiedYears = years.map(function(element) {
+    return 2022 - element;
+});
+console.log(modifiedYears);
+
+
+
+
+
+
+
+
+
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,35 @@
+//filter 
+function()
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const result = odd.filter(getOddNumbers);
+
+function getOddNumbers(num) {
+    return num % 2 !== 0;
+}
+
+//lub 
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const oddNumbers = odd.filter(function(item) {
+    return item[1] !== 2;
+});
+console.log(oddNumbers)
+
+//reduce()
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let result = arr.reduce((accumulator, currentValue) => {
+  if (currentValue % 2 > 0) {
+    return accumulator + currentValue;
+  } else {
+    return accumulator
+  }
+});
+
+console.log(result);
+
+
+
+
+
+
+
