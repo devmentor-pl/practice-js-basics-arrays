ee3dfd02555d36060dd63c631384355a3ec2a193
Anna <aniamatrackasadurek@gmail.com>
2022-11-04 09:27:54 +0100

    exercise 5 changed

--- 05/app.js
+++ 05/app.js
@@ -3,7 +3,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 const getSumOfEvenNumbers = numbers.filter( isEven ).reduce( function(a, b) {
     return a + b;
-});
+}, 0);
 
 function isEven(number) {
     return number % 2 === 0;
