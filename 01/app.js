5b30d3ee8fe990ea047c7fb523bed37d37c82d5c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-19 18:54:14 +0100

    Finish task 1

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const users = [
+	'Jan Kowalski',
+	'Anna Nowak',
+	'Maja Kowalska',
+	'Janusz Nowak',
+	'Tadeusz Dobrodziej',
+	'Mateusz Mateusz',
+];
+
+console.log(users[0], users[2], users[4]);
+console.log(users.length);
