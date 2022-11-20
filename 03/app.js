ab48c6af2a2602955bcc1b6b502a48c7e716e578
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-20 17:54:52 +0100

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,15 @@
-const n = 24;
+// const n = 0;
+// const n = 1;
+const n = 100;
 const oddNumbers = [];
+console.log(addOddNumbers(n));
+
+function addOddNumbers(max) {
+	while (oddNumbers.length < max) {
+		let randomNumber = Math.round(Math.random() * max);
+		if (randomNumber % 2 !== 0) {
+			oddNumbers.push(randomNumber);
+		}
+	}
+	return oddNumbers;
+}
