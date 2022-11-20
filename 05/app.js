dcacdf0540f474ccb54e44445e745160c8c554e6
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-20 18:34:10 +0100

    Finish task 5

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const sumEvenNumbers = numbers.filter(checkEvenNumbers).reduce(sumNumbers, 0);
+console.log(sumEvenNumbers);
+
+function checkEvenNumbers(number) {
+	if (number % 2 === 0) {
+		return number;
+	}
+}
+
+function sumNumbers(sum, el) {
+	return sum + el;
+}
