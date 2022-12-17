2ec3a62fd212aef78517bad239b45fbfbcb35c0e
Michal Filipowski <mikefilipowski@gmail.com>
2022-12-17 17:14:17 +0100

    js-basics-arrays done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter((element) => {
+    element = element % 2 === 0;
+    return element;
+});
+
+const initialValue = 0;
+const sumEvenNumbers = evenNumbers.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+  
+console.log(sumEvenNumbers);
