0d1fde5f54e8058c424d45e6e03808256eb27515
Michal Lata <maxpoz3212@gmail.com>
2022-12-21 18:26:50 +0100

    Add even numbers from array sum program

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function (item) {
+    if (item % 2 === 0) {
+        return item;
+    }
+})
+console.log(evenNumbers);
+
+// Musiałem chwilę pogłówkować nad zasadą działania reduce, ale przy wsparciu dokumentacji + podobnego pytania na slacku mniej więcej zrozumiałem jak działa ta metoda
+const sumEvenNumbers = evenNumbers.reduce(function (total, num) {
+    return total + num;
+})
+
+console.log(sumEvenNumbers);
