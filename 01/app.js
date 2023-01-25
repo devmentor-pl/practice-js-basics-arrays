3c10734bb48f9ad3ae022ab7864d82152edac412
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-01-25 17:32:27 +0100

    task-01 done

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const users = [
+	'Jan Kowalski',
+	'Karol Jankowski',
+	'Karolina Nowak',
+	'Daria Kowalska',
+	'Dariusz Sobota',
+	'Michał Anioł',
+]
+
+console.log(users[0])
+console.log(users[2])
+console.log(users[4])
+console.log(users.length);
