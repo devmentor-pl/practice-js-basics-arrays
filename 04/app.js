ed5afd5eee9754cd9e5d354710470659fa7f3606
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-01-31 22:35:42 +0100

    Update task-04

--- 04/app.js
+++ 04/app.js
@@ -1,6 +1,7 @@
 const years = [1980, 1934, 2002, 2019]
 
+const currentYear = new Date().getFullYear()
 const countYears = years.map(function (el) {
-	return 2023 - el
+	return currentYear - el
 })
 console.log(countYears)
