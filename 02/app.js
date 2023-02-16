a124ee1dca9c08e5997a939588902a2f0342636e
Wioleta Lipinska <hello@wiolipcreates.pl>
2023-02-16 23:18:28 +0100

    task 02

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,6 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
 
-
-
 // nie modyfikuj kodu poniżej!
 
 // funkcję może deklarować poniżej wywołania
@@ -21,3 +19,15 @@ function createRandomArray() {
 function getRandomInteger(min, max) {
     return Math.round(Math.random() * (max-min) + min);
 }
+
+
+for (let i=0; i<randomArray.length; i++){
+    console.log([i]);
+}
+
+randomArray.forEach((el, i, arr)=>{
+    console.log([i]);
+})
+
+
+console.log(randomArray[randomArray.length-1]);
