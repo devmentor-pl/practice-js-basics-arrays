10916c7cd2f045f17cb83ac78e8792de93aa6015
Wioleta Lipinska <hello@wiolipcreates.pl>
2023-02-17 21:07:22 +0100

    task05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenItem = numbers.filter(checkNumbers);
+
+function checkNumbers(numbers){
+    return numbers%2===0;
+
+}
+
+console.log(evenItem);
+
+const sum = evenItem.reduce(function(acc, curr){
+    return acc + curr ;
+})
+console.log(sum);
