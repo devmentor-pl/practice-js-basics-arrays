f9b27b3fb734e7fa462e417411eb0dc0d3333921
Krystian O <krystian.ostrogorski@gmail.com>
2023-02-22 13:01:43 +0100

    tasks finished

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+const date = new Date().getFullYear();
+
+const years2 = years.map((element) => {
+  return date - element;
+});
+
+console.log(years2);
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = (evenNumber) => {
+  return evenNumber % 2 == 0;
+};
+
+const add = (result, el) => {
+  return result + el;
+};
+
+const filterArray = numbers.filter(evenNumbers);
+const reduceArray = filterArray.reduce(add);
+
+console.log(filterArray);
+console.log(reduceArray);
