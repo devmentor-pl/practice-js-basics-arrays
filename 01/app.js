0b1d502d965f7001edb9a9da97cb54bd7560aa47
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-03-04 14:55:17 +0100

    finished 1-3

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+const users = ["Jan Salata", "Tom Forfitter", "John Snow", "Andrzej Duda", "Geralt z Rivii"]
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log("Items in table: " + users.length);
