a0eb1c86c570eb5f6263f8c792188bf1bafa981e
Paweł Niedużak <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-03-14 22:15:43 +0100

    feat: finish tasks

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,16 @@
-const n = 24;
+const n = 100;
 const oddNumbers = [];
+
+let i = 1;
+do {
+	if (n === 0) {
+		console.warn('Maximal value of loop is too low');
+	}
+	if (i % 3 === 0) {
+		oddNumbers.push(i);
+	}
+	i++;
+} while (i <= n);
+
+
+console.log(oddNumbers);
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+let sum = 0;
+const newArr = numbers.filter(number => number % 2 === 0);
+const sumFromArr = newArr.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sumFromArr);
