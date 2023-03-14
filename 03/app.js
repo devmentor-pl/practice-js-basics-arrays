6b5125eb5b1197fca51a92a210ad5c557ccc8e5b
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-14 21:37:52 +0100

    Convert even numbers to odd

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,6 @@ const n = 24;
 const oddNumbers = [];
 
 for (let i = 0; i <= n; i++) {
-    (i % 2 === 0 && i !== 0) ? oddNumbers.push(i) : false;
+    (i % 2 === 1) ? oddNumbers.push(i) : false;
 }
 console.log(oddNumbers)
