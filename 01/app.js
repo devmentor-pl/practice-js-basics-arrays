63de2cda2ff1e45877ac6a71ca317653b911c45d
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-03-16 23:52:49 +0100

    arrays ended

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+const users = ['Jan Kowalski', 'Barbara Krawcówna', 'Remigiusz Rondelek', 'Piotr Czelny', 'Eustachy Rurek']
+
+console.log(users[0])
+console.log(users[2])
+console.log(users[4])
+
+console.log(users.length)
