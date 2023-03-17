92ec22c4d97ebbf9ee846ef453f4ddb334eff2f7
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-17 16:56:03 +0100

    Added task 5

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,20 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(num) {
+    if (!(num % 2)) {
+        return numbers;
+    }
+});
+
+console.log(evenNumbers);
+
+const initialValue = 0;
+
+const sumEvenNumbers = evenNumbers.reduce(function(accumulator, currentValue) {  
+    return accumulator + currentValue;
+});
+
+console.log(sumEvenNumbers);
+
+
+
