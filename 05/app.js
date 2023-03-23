86c8e3c83d546905e5c6f547dedcb44aa5b2c1d7
Ireneusz Cislo <irekc86@o2.pl>
2023-03-23 18:05:32 +0100

    task 05 finished

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+});
+
+console.log('parzyste numery z tablicy =>');
+console.log(evenNumbers);
+
+const sumOfEvenNumbers = evenNumbers.reduce(function(a, b) {
+    return a + b;
+});
+
+console.log('suma parzystych numerów => ');
+console.log(sumOfEvenNumbers);
