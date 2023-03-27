3c62f278de24ed08fefe4cfabcc469f00546a476
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-03-27 23:27:31 +0200

    Task 01 done

---
+++ 01/app.js
@@ -0,0 +1,10 @@
+const users = [
+  "Jan Kowalski",
+  "Janusz Nowak",
+  "Bartosz Noga",
+  "Mariusz Celny",
+  "Magdalena Piątek",
+  "Maciej Losowy"
+];
+console.log(users[0], users[2], users[4]);
+console.log(users.length);
