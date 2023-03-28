c360700270effd75f86c188931c1866d1aea4fbf
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-03-28 15:37:40 +0200

    finish task 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
+
+const yearsDifference = years.map(subtractionYears);
+
+function subtractionYears(e) {
+  const nowYear = new Date().getFullYear();
+  return nowYear - e;
+}
+
+yearsDifference.forEach(function (e) {
+  console.log(e);
+});
