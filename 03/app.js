1640c07fa7ea5636aebff981f9a0586f0655c639
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-04-02 13:48:06 +0200

    Task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,49 @@
 const n = 24;
 const oddNumbers = [];
+
+for (let i = 1; i <= n; i++) {
+  if (i % 2 === 1) {
+    oddNumbers.push(i);
+  }
+}
+
+console.log(oddNumbers);
+
+// TEST DLA n = 0
+
+const n1 = 0;
+const oddNumbers1 = [];
+
+for (let i = 1; i <= n1; i++) {
+  if (i % 2 === 1) {
+    oddNumbers1.push(i);
+  }
+}
+
+console.log(oddNumbers1);
+
+// TEST DLA n = 1
+
+const n2 = 1;
+const oddNumbers2 = [];
+
+for (let i = 1; i <= n2; i++) {
+  if (i % 2 === 1) {
+    oddNumbers2.push(i);
+  }
+}
+
+console.log(oddNumbers2);
+
+// TEST DLA n = 100
+
+const n3 = 100;
+const oddNumbers3 = [];
+
+for (let i = 1; i <= n3; i++) {
+  if (i % 2 === 1) {
+    oddNumbers3.push(i);
+  }
+}
+
+console.log(oddNumbers3);
