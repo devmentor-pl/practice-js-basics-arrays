064cc0b7501d0f389101577b3e310cd79ee784ad
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-04-02 13:55:15 +0200

    Task 03 -  turn tests into comments

--- 03/app.js
+++ 03/app.js
@@ -9,41 +9,41 @@ for (let i = 1; i <= n; i++) {
 
 console.log(oddNumbers);
 
-// TEST DLA n = 0
+// <-- TEST DLA n = 0 -->
 
-const n1 = 0;
-const oddNumbers1 = [];
+// const n1 = 0;
+// const oddNumbers1 = [];
 
-for (let i = 1; i <= n1; i++) {
-  if (i % 2 === 1) {
-    oddNumbers1.push(i);
-  }
-}
+// for (let i = 1; i <= n1; i++) {
+//   if (i % 2 === 1) {
+//     oddNumbers1.push(i);
+//   }
+// }
 
-console.log(oddNumbers1);
+// console.log(oddNumbers1);
 
-// TEST DLA n = 1
+// <-- TEST DLA n = 1 -->
 
-const n2 = 1;
-const oddNumbers2 = [];
+// const n2 = 1;
+// const oddNumbers2 = [];
 
-for (let i = 1; i <= n2; i++) {
-  if (i % 2 === 1) {
-    oddNumbers2.push(i);
-  }
-}
+// for (let i = 1; i <= n2; i++) {
+//   if (i % 2 === 1) {
+//     oddNumbers2.push(i);
+//   }
+// }
 
-console.log(oddNumbers2);
+// console.log(oddNumbers2);
 
-// TEST DLA n = 100
+// <-- TEST DLA n = 100 -->
 
-const n3 = 100;
-const oddNumbers3 = [];
+// const n3 = 100;
+// const oddNumbers3 = [];
 
-for (let i = 1; i <= n3; i++) {
-  if (i % 2 === 1) {
-    oddNumbers3.push(i);
-  }
-}
+// for (let i = 1; i <= n3; i++) {
+//   if (i % 2 === 1) {
+//     oddNumbers3.push(i);
+//   }
+// }
 
-console.log(oddNumbers3);
+// console.log(oddNumbers3);
