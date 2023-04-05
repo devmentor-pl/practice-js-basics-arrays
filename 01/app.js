0bde2ee735bcac2958fb803744f0fa4213364161
Maria Kardas <mariah.grze@gmail.com>
2023-04-05 09:54:47 +0200

    Finished task 01

--- 01/app.js
+++ 01/app.js
@@ -1,6 +1,6 @@
 let users = ['Maria Mariowa', 'Kasia Kasiowa', 'Basia Basiowa', 'Marcin Marcinowy', 'Kamil Banan']
 
-console.log(users)
+console.log(users) // ADDITIONAL
 
 console.log(users[0], users[2], users[4])
 
