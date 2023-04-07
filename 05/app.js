84582c8e35795349a874338f02c7ca149742970c
kozerka <kozerka@gmail.com>
2023-04-07 20:35:18 +0200

    Complete task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function (number) {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+const evenSum = evenNumbers.reduce((acc, cur) => acc + cur, 0);
+console.log(evenSum);
