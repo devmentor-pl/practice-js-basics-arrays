18d5c650009ba640f8e17eaf62ca0197648969ee
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-25 15:48:37 +0200

    improve task 4

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
 
+const currYear = (new Date().getFullYear())
 const differenceYears = years.map(function(element) {
-    return 2023 - element;
+    return currYear - element;
 });
 
 console.log(differenceYears);
