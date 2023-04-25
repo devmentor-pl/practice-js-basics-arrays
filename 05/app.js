692c4343a18e77e8ec3de3cd18b48fca2716e468
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-25 15:59:55 +0200

    task 5 improve

--- 05/app.js
+++ 05/app.js
@@ -3,6 +3,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7];
 const evenNumbers = numbers.filter(function(num) {
     if (!(num % 2)) {
         return numbers;
+        // można zapisać tylko 'return num % 2 === 0' i wtedy też od razu zwraca element
     }
 });
 
