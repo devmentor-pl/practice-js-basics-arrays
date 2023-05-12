9013712d793a15581b7812a7541238d3e40763b4
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-05-12 22:00:12 +0200

    Finish tasks

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,11 @@
 const years = [1980, 1934, 2002, 2019];
+const currentDate = new Date().getFullYear()
+
+const yearsResult = countYears(years, currentDate)
+console.log(yearsResult);
+
+function countYears(yrs, currentYr) {
+    const newArr = yrs.map(function (yr) { return currentYr - yr })
+    return newArr
+}
+
