9da23168d9f7dfd1fae7ae43fe8911cc961ada45
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-05-13 19:03:30 +0200

    Improve js

--- 03/app.js
+++ 03/app.js
@@ -1,17 +1,16 @@
 const n = 100;
-const oddNumbers = [];
+const oddNumbers = getOddNumbers(n);
 
-getOddNumbers(n)
 console.log(oddNumbers);
 
-
 function getOddNumbers(n) {
-
+    const temp = []
     for (let i = 1; i <= n; i++) {
-        if (i%2) {
-            oddNumbers.push(i)
+        if (i % 2) {
+            temp.push(i)
         }
     }
+    return temp
 }
 
 
--- 04/app.js
+++ 04/app.js
@@ -1,11 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
-const currentDate = new Date().getFullYear()
 
-const yearsResult = countYears(years, currentDate)
+const yearsResult = countYears(years)
 console.log(yearsResult);
 
-function countYears(yrs, currentYr) {
-    const newArr = yrs.map(function (yr) { return currentYr - yr })
+function countYears(yrs) {
+    const currentYear = new Date().getFullYear()
+
+    const newArr = yrs.map(function (yr) { return currentYear - yr })
     return newArr
 }
 
