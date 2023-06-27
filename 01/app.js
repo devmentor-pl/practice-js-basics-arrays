08a2513be75938ce315d9dfbe10541f84395d71b
anetasobanska <sannettka@gmail.com>
2023-06-27 15:19:14 +0200

    finish 01

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+const users = ['Alicja Janosz', 'Ewelina Flinta', 'Szymon Wydra', 'Malgorzata Stepien', 'Tomasz Makowiecki'];
+
+console.log('tablica zawiera ' + users.length + ' elementow');
+console.log('user 1:', users[0]);
+console.log('user 3:', users[2]);
+console.log('user 5:', users[4]);
