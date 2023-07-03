9afdd2aef4130e3ccccdefe3174da23b8fac598a
anetasobanska <sannettka@gmail.com>
2023-07-03 06:47:17 +0200

    new array added in 04

--- 04/app.js
+++ 04/app.js
@@ -2,6 +2,6 @@ const years = [1980, 1934, 2002, 2019];
 
 let date =  new Date().getFullYear();
 
-years.forEach(year => {
-    console.log(date - year);
-});
+const yearsNewArr = years.map(year => date - year);
+
+console.log(yearsNewArr);
