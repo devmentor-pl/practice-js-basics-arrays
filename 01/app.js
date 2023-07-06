222418f21d9e19ccf9803f85fd0ddd4752925c38
Marcin Soltysik <profittax@gmail.com>
2023-07-06 23:23:49 +0200

    Done 01, arrays

--- 01/app.js
+++ 01/app.js
@@ -1,7 +1,3 @@
-/* W pliku `app.js` stwórz zmienną `users` i przypisz do niej tablicę, która będzie przechowywać imiona i nazwiska przedstawione w formie ciągu znaków.
-
-Następnie wyświetl w konsoli informacje o pierwszym, trzecim i piątym użytkowniku. Dodatkowo z pomocą właściwości `.length` wyświetl, ile elementów zawiera tablica. */
-
 const users = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
 console.log(users[0], users[2], users[4]);
