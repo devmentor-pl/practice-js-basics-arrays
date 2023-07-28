c2e0e626e546d9ec81c96aa037edfd8de1ae9821
Marcin <xcinek1337@wp.pl>
2023-07-28 16:18:24 +0200

    01

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+let users = ['Jan Kowalski', 'Anna Nowak', 'Marek Nowicki', 'Ewa Wiśniewska', 'Tomasz Lewandowski'];
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log('ilczba indexów w tablicy: ', users.length);
