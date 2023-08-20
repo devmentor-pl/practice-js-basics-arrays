ec0adb741b9b9e05f5304c991797136d7196cb7b
Hubert Wojnowski <hubertwojnowski0@gmail.com>
2023-08-20 17:08:51 +0200

    Finish 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+
+const currYear = (new Date()).getFullYear();
+const ages = years.map(function(item, index, array){
+    return currYear - item;
+});
+
+console.log(ages)
