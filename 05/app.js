df8965fee94d80b63f7e09ca51119afb9b8569ac
Marcin Chechelski <marcin.chechelski@op.pl>
2023-09-13 22:05:04 +0200

    task done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = getEvenNumbers(numbers); // [2, 4];
+const sum = getSum(evenNumbers); // 2 + 4 = 6;
+console.log(sum); // 6;
+function getEvenNumbers(arr) {
+    const evenNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenNumbers.push(arr[i]);
+        }
+    }
+    return evenNumbers;
+}
