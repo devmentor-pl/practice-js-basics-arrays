21e4472a9a67f9eb8fcc98957f308203df5ce784
Marcin Chechelski <marcin.chechelski@op.pl>
2023-09-14 21:57:44 +0200

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,10 @@
 const n = 24;
 const oddNumbers = [];
 
-// if (n >= 1) {
-//     for (let i = 1; i <= n; i++) {
-//         if (i % 2 !== 0) {
-//             oddNumbers.push(i);
-//         }
-//     }
-// }
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 1) {
+        oddNumbers.push(i);
+    }
+}
 
-// console.log(oddNumbers);
+console.log(oddNumbers);
