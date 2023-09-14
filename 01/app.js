fedc697cdb031e85c9287307bfc27736099f12ed
Maciej Necka <macieknecka@gmail.com>
2023-09-14 16:23:56 +0200

    Task done

---
+++ 01/app.js
@@ -0,0 +1,10 @@
+const users = [
+  'Marta Nowak',
+  'Adam Kowalski',
+  'Natalia Jankowska',
+  'Michał Wiśniewski',
+  'Anna Szymańska',
+];
+
+console.log(users[0], users[2], users[4]);
+console.log(users.length);
