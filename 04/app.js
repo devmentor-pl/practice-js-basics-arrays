85beb8ef902c2c18849b6ecb3eaab2d91c98ea1f
Kasia <k.kasia912@gmail.com>
2023-11-16 17:27:50 +0100

    complete task 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,11 @@
 const years = [1980, 1934, 2002, 2019];
+
+function checkYears() {
+  const actualYear = new Date().getFullYear();
+  const diffYears = years.map((yr) => actualYear - yr);
+  return diffYears;
+}
+// console.log(checkYears());
+
+const diffYears = checkYears();
+console.log(diffYears);
