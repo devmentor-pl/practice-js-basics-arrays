63b685a177c480c38642fd5e1197aa9c92ef9bdf
Lukasz <l.bdnski@gmail.com>
2023-12-08 13:58:07 +0100

    add task

---
+++ 01/app.js
@@ -0,0 +1,3 @@
+const users = ['Leonardo DiCaprio', 'Michael Jordan', 'Ronaldo', 'Messi', 'Usain Bolt', 'MAradona'];
+
+console.log(`Selected users: ${users[0]}, ${users[2]}, ${users[4]},`, `from: ${users.length} users list`);
