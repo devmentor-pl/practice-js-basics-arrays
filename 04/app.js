c8ff299621c2034d39beb77cdd0712eee67710fc
Dawidtk <dawidtk@gmail.com>
2023-12-15 18:22:44 +0100

    done

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,7 @@
 const years = [1980, 1934, 2002, 2019];
 
-const ages = years.map(function(item, index) {
-    return index;
-})
+const ages = years.map(function(item) {
+    return 2023 - item;
+});
+
+console.log(ages);
