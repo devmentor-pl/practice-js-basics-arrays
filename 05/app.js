3396e466320efa42eab1ce7b869a2cd218325681
Dawidtk <dawidtk@gmail.com>
2023-12-16 18:37:30 +0100

    done

--- 05/app.js
+++ 05/app.js
@@ -1,8 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const sumNumbers = numbers.filter(elemen);
+const sumNumbers = numbers.filter(getNumbers);
 console.log(sumNumbers);
 
-const sumOfNumbers = sumNumbers.reduce(sumFilteredNumbers);
+const sumOfNumbers = sumNumbers.reduce(sumFilteredNum);
 console.log(sumOfNumbers);
 
+function getNumbers(elNumbers) {
+    return elNumbers % 2 === 1;
+}
+
+function sumFilteredNum(tot, num) {
+    return tot + num;
+}
