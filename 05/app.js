73f957ef199f0ef87ac54d566ab468c5efe6f91c
Dawidtk <dawidtk@gmail.com>
2023-12-16 18:20:45 +0100

    reduce in progress

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const sumNumbers = numbers.filter(function);
+const sumNumbers = numbers.filter(elemen);
 console.log(sumNumbers);
+
+const sumOfNumbers = sumNumbers.reduce(sumFilteredNumbers);
+console.log(sumOfNumbers);
+
