80874e5e527ef9d73cb22a423fb9dd7403b69291
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-19 20:54:00 +0100

    Add informations about users in array

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+const users = ['Zbigniew Stonoga', 'Adam Małysz', 'Robert Kubica', 'Dariusz Kowalski', 'Ryszard Nowak', 'Łukasz Pieczarka'];
+
+console.log(users[0], users[2], users[4]);
+
+console.log(users.length);
