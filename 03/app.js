bc361ec456386eb1e2aaab16ea9651dd3cd766e7
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-05 12:20:33 +0100

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,12 @@
-const n = 24;
+const n = 51;
 const oddNumbers = [];
+fillArray();
+console.log(oddNumbers);
+
+function fillArray () {
+  for (let i = 1; i <= n; i++) {
+    if (!(i % 2 === 0)) {
+      oddNumbers.push(i);
+    }
+  }
+}
