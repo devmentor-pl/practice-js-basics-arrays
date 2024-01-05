3643227c99f6b0d8b86a0bc47fdc6e94ab0ebdf0
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-05 12:55:59 +0100

    Finish 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+
+const passedYears = years.map(function(a, b) {
+  const compared = years[++b];
+  return Math.abs(a - compared);
+});
+
+console.log(passedYears);
