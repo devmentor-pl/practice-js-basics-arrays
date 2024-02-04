c2453beab84495dbe2d1b78b30a698677d862f93
Rafal <rafk528@gmail.com>
2024-02-04 18:39:22 +0000

    oddNumbers Array

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,40 @@
-const n = 24;
-const oddNumbers = [];
+console.log("n = 24: ", createOddNumbersArray(24))
+console.log("-------------------------")
+
+//Warunki brzegowe
+console.log("n = 0: ", createOddNumbersArray(0))
+console.log("-------------------------")
+
+console.log("n = 1: ", createOddNumbersArray(1))
+console.log("-------------------------")
+
+console.log("n = 100: ", createOddNumbersArray(100))
+console.log("-------------------------")
+
+console.log("n < 0: ", createOddNumbersArray(-1))
+console.log("-------------------------")
+
+console.log("n = 'string': ", createOddNumbersArray("string"))
+console.log("-------------------------")
+
+function createOddNumbersArray(n) {
+  const oddNumbers = []
+  console.log(n)
+
+  if (typeof n !== "number") {
+    console.error("n must be number")
+    return oddNumbers
+  }
+
+  if (n < 0) {
+    console.error("value of n must be a non-negative number")
+    return oddNumbers
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+      oddNumbers.push(i)
+    }
+  }
+  return oddNumbers
+}
