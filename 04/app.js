3f8d4266f9871d95bdd62dbe7eb6c90d590b202e
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-14 13:35:36 +0100

    04 completed

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
+const n = 2023;
+
+const newYears = years.map(showTimeLapse);
+console.log(newYears);
+
+function showTimeLapse(element) {
+    let newElement = 0;
+    newElement = n - element; 
+
+    return newElement;
+}
