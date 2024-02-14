35d3724bf701c779523d6ae6d8e2d7168a53e342
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-14 14:01:43 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
+//  Kiedy wykonujemy 'return' wewnątrz funkcji ?
+// jest gdzieś błąd
+
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = numbers.filter(sortEvenNumbers);
+
+function sortEvenNumbers(numbers) {
+    if(numbers % 2 === 0) {
+        // evenNumbers.push(number);
+    }
+    return numbers;
+    // return evenNumbers;
+}
