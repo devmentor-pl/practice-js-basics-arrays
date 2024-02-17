d408fd191b25bf81e7eafd1e1b8cc635b9e17f67
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-17 22:18:23 +0100

    05 completed

--- 05/app.js
+++ 05/app.js
@@ -1,15 +1,12 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
-const evenNumbers = [];
+let evenNumbers = [];
 sortEvenNumbers(numbers);
 
 function sortEvenNumbers(num) {
     if(num % 2 === 0) {
-        evenNumbers.push(num);
+        return evenNumbers;
     }
-    return evenNumbers;
 }
-
-console.log(evenNumbers);
 
 function getSum(arr) {
     let sum = 0;
@@ -21,29 +18,8 @@ function getSum(arr) {
     return sum;
 }
 
-const sumNumbers = getSum(numbers);
-console.log(sumNumbers);
+evenNumbers = numbers.filter(sortEvenNumbers);
+console.log(evenNumbers);
+
 const sumEvenNumbers = getSum(evenNumbers);
 console.log(sumEvenNumbers);
-
-
-// // const evenNumbers = numbers.filter(sortEvenNumbers);
-
-// // wyświetlanie elementów tablicy
-// numbers.forEach( function(el) {
-//     console.log(el);
-// })
-// // dodawanie 1 do kadego elementu tablicy
-// let newArr = [];
-// numbers.forEach( function(el) {
-//     newArr.push(el + 1);
-// })
-// console.log(newArr);
-
-// // zliczanie ilości liter w tablicy słów
-// const stringArr = ['Kasia', 'Ania', 'Basia'];
-// let letterSum = 0;
-// stringArr.forEach( function(word) {
-//     letterSum += stringArr.length + 1;
-// })
-// console.log(letterSum);
