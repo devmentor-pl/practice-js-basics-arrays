17ef2977d379649cfa066b0793b72ba5c2073b65
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-03-05 18:29:38 +0100

    add tasks arrays

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
+
+
+const yearsPeriod = years.map(timePeriod);
+console.log(yearsPeriod);
+
+function timePeriod(item) {
+    return 2024 - item;
+}
--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,21 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbersArray = numbers.filter(evenNumbers);
+console.log(evenNumbersArray);
+
+function evenNumbers(element) {
+    if (element % 2 === 0) {
+        return element
+    }
+}
+
+const initialValue = 0;
+
+const sumEvenNumbers = evenNumbersArray.reduce(sumNumbers);
+
+
+function sumNumbers(item, currentValue) {
+    return item + currentValue
+}
+
+console.log(sumEvenNumbers);
