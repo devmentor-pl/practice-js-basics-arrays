b3bf61fb29e23f3db5b5b4c21ed0c1bded34a0ba
Jarek Stelmach <j.stelmach@onet.eu>
2024-03-07 18:00:39 +0100

    Finished ex 1 in arrays

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+const users = ["Jan Kowalski", "Marta Kieliszek", "Zenon Gminny", "Albert Sosna", "Michał Bułka"]
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
+console.log("Tablica zwiera =", users.length);
