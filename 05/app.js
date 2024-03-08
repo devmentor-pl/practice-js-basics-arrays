78580ddf7c85743b1144c8f234c1b65f966f0622
Jaro <jaroswidnik@interia.pl>
2024-03-08 10:19:37 +0100

    Finished ex 5 in arrays

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(el) {
+    return el % 2 === 0
+})
+
+console.log("Tablica z liczbami parzystymi", evenNumbers);
+
+const sumEvenNumbers = evenNumbers.reduce(function(a, b) {
+    return a + b;
+}, 0);
+
+console.log("Suma liczb parzystych to", sumEvenNumbers);
