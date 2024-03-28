a6c3ddb1c98d8dbf79fb89bf5d4ce1378700f0dd
Emilia Szafarska <emilia.anna.szafarska@gmail.com>
2024-03-28 17:54:27 +0100

    Add solution to task 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,15 @@
 const n = 24;
 const oddNumbers = [];
+
+function getOddNumbers() {
+  for(let i = 0; i <= n; i++) {
+    if(i % 2 !== 0) {
+      oddNumbers.push(i)
+    }
+  }
+  return oddNumbers;
+}
+
+getOddNumbers();
+console.log('Liczby nieparzyste w określonej wartiości to:');
+console.log(oddNumbers);
