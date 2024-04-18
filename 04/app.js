ff3c00403ba26a4eb08fc9a830b53375a1414457
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-04-18 15:29:43 +0200

    Task 04 - const added to variable currentYear

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
-currentYear = (new Date).getFullYear();
+const currentYear = (new Date).getFullYear();
+
 console.log(currentYear);
 
 const yearsPassed = years.map(function(element) {
     return currentYear - element;
 });
-
 console.log(yearsPassed);
