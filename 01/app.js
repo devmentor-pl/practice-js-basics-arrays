d08d59513e83dec1d463351dc785ce38929e95d8
BartekB94 <wurir1994@gmail.com>
2024-05-06 18:50:46 +0200

    complete 01

---
+++ 01/app.js
@@ -0,0 +1,5 @@
+const users = ['Bartosz Bogdanowicz', 'Krzysiek Krawczyk', 'Tomek Przydomek', 'Janina Kalinowska', 'Katarzyna Dziewczyna'];
+
+console.log(users[0], users[2], users[4]);
+console.log(users.length)
+
