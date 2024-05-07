536a51c220b5cfa74b5d6cfaf991d14587ed21f2
BartekB94 <wurir1994@gmail.com>
2024-05-07 08:25:02 +0200

    complete 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(findEvenNumbers).reduce(addNum);
+console.log(evenNumbers);
+
+function findEvenNumbers(num) {
+    if (num % 2 === 0) {
+        return num
+    }
+};
+
+function addNum(sum, num) {
+    return sum + num
+};
