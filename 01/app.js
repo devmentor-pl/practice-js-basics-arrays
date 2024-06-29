3111065bfa039d6319e99a75de4509de5c9fb4ce
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-06-29 13:50:33 +0200

    Add task 01

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const users = new Array(
+  "Jan Kowalski",
+  "Anna Nowak",
+  "Katarzyna Nowicka",
+  "Adam Kowal",
+  "Joanna Zmyślona"
+);
+
+console.log("Użytkownik pierwszy: " + users[0]);
+console.log("Użytkownik trzeci: " + users[2]);
+console.log("Użytkownik piąty: " + users[4]);
+
+console.log("Tablica zawiera " + users.length + " elementów.");
