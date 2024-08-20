682d6da6f459c4bfaeece117900aad7856ae361f
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-20 23:42:18 +0200

    task 1 completed

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+users = ["Paweł Sikorski", "Adam Niedzielski", "Tomek Kurek", "Aleks Baran", "Magda Sobota"]
+
+const firstElementOfArray = users[0]
+console.log(firstElementOfArray)
+const thirdElementOfArray = users[2]
+console.log(thirdElementOfArray)
+const fifthElementOfArray = users[4]
+console.log(fifthElementOfArray)
+
