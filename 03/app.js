8a6987ca3f5523de1cc7330da277f051117687fe
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-21 00:05:56 +0200

    task 3 completed

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,17 @@
 const n = 24;
 const oddNumbers = [];
+
+function findRandomOddNumberInGivenRange(range) {
+    let number = 0;
+    while((number % 2 === 0)) {
+        number = Math.round(Math.random() * range)
+    }
+    return number
+}
+
+for(let i = 0; i < n; i++) {
+    const number = findRandomOddNumberInGivenRange(n)
+    oddNumbers.push(number)
+    console.log(oddNumbers[i])
+}
+
