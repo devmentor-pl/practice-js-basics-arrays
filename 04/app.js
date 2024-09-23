9605f3b055b33856d38c7bdf13db22ca101d9b4d
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-23 16:38:20 +0200

    added solution to 4 task

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,5 @@
 const years = [1980, 1934, 2002, 2019];
+
+const onlyEvenNumber = years.filter(number => number % 2 === 0);
+const sum = onlyEvenNumber.reduce((number, currentValue) => number + currentValue, 0);
+console.log(sum);
