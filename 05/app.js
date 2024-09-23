1044e3582b3d1d90ee30bbd4a0b08198ea6e7f67
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-23 16:40:27 +0200

    added solution to 5 task

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const onlyEvenNumber = numbers.filter(number => number % 2 === 0);
+const sum = onlyEvenNumber.reduce((number, currentValue) => number + currentValue, 0);
+console.log(sum);
