48c24b6fabe8b9769ed0a076d80868e52353a36a
marrcelp <marcel.piaszczyk@gmail.com>
2024-11-26 20:35:21 +0100

    resolved exercise 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,22 @@
 const n = 24;
 const oddNumbers = [];
+
+// for (let i = 0; i < n; i++){
+//     if(i%2 !== 0) {
+//         oddNumbers.push(i);
+//     }
+// }
+// console.log(oddNumbers);
+
+function showOddNumbers(maxValue){
+    let arr = [];
+
+    for (let i = 0; i < maxValue; i++){
+        if(i%2 !== 0) {
+            arr.push(i);
+        }
+    }
+    console.log(arr);
+}
+
+showOddNumbers(n);
