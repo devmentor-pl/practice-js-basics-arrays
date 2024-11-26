2651e279c7635d4a064ce72e0c34c1710243acb2
marrcelp <marcel.piaszczyk@gmail.com>
2024-11-26 20:43:38 +0100

    resolved ex 4

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,7 @@
 const years = [1980, 1934, 2002, 2019];
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+const yearsToDate = years.map((element) => currentYear - element);
+console.log(yearsToDate);
