eb008e13dbbdb64707db5b6ff714bf3bd3061c72
marrcelp <marcel.piaszczyk@gmail.com>
2024-12-04 17:35:51 +0100

    add second parametr - 0 to reduce

--- 05/app.js
+++ 05/app.js
@@ -12,6 +12,6 @@ const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 
 const evenNumbers = numbers.filter((element) => element%2===0);
-const sum = evenNumbers.reduce((acc, el) => acc+ el);
+const sum = evenNumbers.reduce((acc, el) => acc + el, 0);
 console.log(sum);
 
