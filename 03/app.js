932b13e49fa1945d5bc57c49b8947e4a70dd046a
Gabriel Macierzyński <gabrysm97@gmail.com>
2025-04-03 17:33:02 +0200

    solution to task 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,10 @@
-const n = 24;
+const n = 100;
 const oddNumbers = [];
+
+for(let i = 0; i <= n; i++){
+    if( i > 0 && i % 2 ===1){
+        oddNumbers.push(i);
+    }
+}
+
+console.log(oddNumbers);
