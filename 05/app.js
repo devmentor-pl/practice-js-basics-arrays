f4be95215980542b74826e3d10f9ea245681e358
Zaneta Sochon <twj email>
2025-04-04 20:38:01 +0200

    feat(5): Display sum of even array numbers

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const result = numbers.filter(EvenNumbers);
+
+function EvenNumbers(evenNumber) {
+  return evenNumber % 2 === 0;
+}
+
+const sumNumbers = 0;
+const sumEvenNumbers = result.reduce((acc, curr) => acc + curr, sumNumbers);
+
+console.log(sumEvenNumbers);
