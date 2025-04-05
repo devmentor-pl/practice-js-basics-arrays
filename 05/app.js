57cc85df4f40f863a22b0796bed36eae0a369f41
Zaneta Sochon <twj email>
2025-04-05 08:49:22 +0200

    fix(5): Change function name tro lowercase

--- 05/app.js
+++ 05/app.js
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
-const result = numbers.filter(EvenNumbers);
+const result = numbers.filter(evenNumbers);
 
-function EvenNumbers(evenNumber) {
+function evenNumbers(evenNumber) {
   return evenNumber % 2 === 0;
 }
 
