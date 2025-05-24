6154fb59b272f149b8d0ae7af5c2d4f08c6ad530
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-25 06:46:32 +1000

    Task 05 done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,23 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+const evenNumbers = getEvenNumbers(numbers);
+const sum = getSum(evenNumbers);
+console.log(sum);
+
+
+function getEvenNumbers(arr) {
+    const evenNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenNumbers.push(arr[i]);
+        }
+    }
+    return evenNumbers;
+}
+
+function getSum(arr) {
+    let sum = 0;
+    for (let j = 0; j < arr.length; j++) {
+        sum += arr[j]
+    }
+    return sum;
+}
