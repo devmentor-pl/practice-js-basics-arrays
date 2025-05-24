96af6c4e554ba3e42a4d1d9a44ac6cfcc8a7cf28
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-24 18:04:45 +1000

    Task 02 done

--- 02/app.js
+++ 02/app.js
@@ -11,13 +11,21 @@ console.log(randomArray);
 function createRandomArray() {
     const arr = [];
     const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
+    for (let i = 0; i < len; i++) {
+        arr.push(getRandomInteger(1, 100));
     }
 
     return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
+
+for (let i = 0; i < randomArray.length; i++) {
+    console.log(randomArray[i]);
+}
+
+randomArray.forEach(function (element, index, array) {
+    console.log(element);
+});
