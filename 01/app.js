1ef961a870ed093eda4c2f6e51689f977069e2df
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-24 17:56:45 +1000

    Task 01 done

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+const users = ['Michal Wasiak', 'Adam Bedka','Karolina Konieczna','Wojtek Zapart','Antoni Waski','Michalina Kowalska'];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log(users.length);
