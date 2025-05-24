34e3cce037f7e81bdee5a978a4d5f8a995c6fd3f
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-25 07:34:31 +1000

    Fix task 05 use reduce() and filter()

--- 05/app.js
+++ 05/app.js
@@ -1,23 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
-const evenNumbers = getEvenNumbers(numbers);
-const sum = getSum(evenNumbers);
+const evenNumbers = numbers.filter((item) => item % 2 === 0);
+const sum = evenNumbers.reduce((item, x) => item + x)
+console.log(evenNumbers);
 console.log(sum);
 
-
-function getEvenNumbers(arr) {
-    const evenNumbers = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-            evenNumbers.push(arr[i]);
-        }
-    }
-    return evenNumbers;
-}
-
-function getSum(arr) {
-    let sum = 0;
-    for (let j = 0; j < arr.length; j++) {
-        sum += arr[j]
-    }
-    return sum;
-}
