35543bcbec647241947f779d7f8e47b6f37a2c30
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-24 20:27:57 +1000

    Task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,11 @@
 const n = 24;
 const oddNumbers = [];
+console.log(oddNumbers);
+
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+        oddNumbers.push(i);
+    }
+}
+
+console.log(oddNumbers);
