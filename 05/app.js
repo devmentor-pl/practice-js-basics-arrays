210b51d43f45436113615fcfeb0fd456c05fed2b
Michal Wasiak <michal.wasiak@hotmail.com>
2025-05-27 16:47:21 +1000

    Task 05 add starting value '0'

--- 05/app.js
+++ 05/app.js
@@ -1,6 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 const evenNumbers = numbers.filter((item) => item % 2 === 0);
-const sum = evenNumbers.reduce((item, x) => item + x)
+const sum = evenNumbers.reduce((item, x) => item + x, 0)
 console.log(evenNumbers);
 console.log(sum);
 
