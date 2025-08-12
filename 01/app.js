20c67e95dc1bc41d3c4fee47f10411c773f55e8b
Natalia Kur <natalia.kur@yahoo.com>
2025-08-12 17:18:33 +0200

    JS arrays: complete task 01

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const users = [
+  "Anna Kowalska",
+  "Patrycja Markowska",
+  "Kuba Sienkiewicz",
+  "Mikołaj Kopernik",
+  "Wisława Szymborska",
+];
+console.log(
+  `Pierwszy użytkownik: ${users[0]}, Drugi użytkownik: ${users[2]}, Trzeci użytkownik: ${users[4]}`
+);
+console.log(`Długość tablicy: ${users.length}`);
