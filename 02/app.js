5dd2356307aac31d3699d1e653e263277009c6d1
Natalia Kur <natalia.kur@yahoo.com>
2025-08-12 18:46:13 +0200

    JS arrays: complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,23 +1,30 @@
 const randomArray = createRandomArray();
 console.log(randomArray);
+for (let i = 0; i < randomArray.length; i++) {
+  console.log(`Result of for loop: ${randomArray[i]}`);
+}
 
-
-
+randomArray.forEach(function (el) {
+  console.log(`Result of forEach: ${el}`);
+});
+console.log(
+  `Last element of Random Array: ${randomArray[randomArray.length - 1]}`
+);
 // nie modyfikuj kodu poniżej!
 
 // funkcję może deklarować poniżej wywołania
 // ponieważ w JS występuje mechanizm tzw. hoisting-u
 
 function createRandomArray() {
-    const arr = [];
-    const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
-    }
+  const arr = [];
+  const len = getRandomInteger(1, 10);
+  for (let i = 0; i < len; i++) {
+    arr.push(getRandomInteger(1, 100));
+  }
 
-    return arr;
+  return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
