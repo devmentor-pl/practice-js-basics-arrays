a2d5eb15ba5c72d791e8cb177aaccad442f744ad
Natalia Kur <natalia.kur@yahoo.com>
2025-08-13 09:16:23 +0200

    JS arrays: complete task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function (el) {
+  return el % 2 === 0;
+});
+const evenNumbersSum = evenNumbers.reduce(function (sum, el) {
+  return sum + el;
+}, 0);
+
+console.log(evenNumbersSum);
