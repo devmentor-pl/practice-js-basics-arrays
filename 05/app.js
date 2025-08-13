e3adfd558823bb28c46e35ca8f19a8f59e4aea60
Natalia Kur <natalia.kur@yahoo.com>
2025-08-13 09:17:48 +0200

    JS arrays task 05: wrap code in function declaration

--- 05/app.js
+++ 05/app.js
@@ -1,10 +1,12 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const evenNumbers = numbers.filter(function (el) {
-  return el % 2 === 0;
-});
-const evenNumbersSum = evenNumbers.reduce(function (sum, el) {
-  return sum + el;
-}, 0);
-
-console.log(evenNumbersSum);
+function sumEvenNumbers() {
+  const evenNumbers = numbers.filter(function (el) {
+    return el % 2 === 0;
+  });
+  const evenNumbersSum = evenNumbers.reduce(function (sum, el) {
+    return sum + el;
+  }, 0);
+  console.log(evenNumbersSum);
+}
+sumEvenNumbers();
