05d5f7990b55b7fd86c1a289abbd5a6f236c5516
Natalia Kur <natalia.kur@yahoo.com>
2025-08-14 20:58:56 +0200

    Task 05: refactor the code

--- 05/app.js
+++ 05/app.js
@@ -2,6 +2,6 @@ const sumEvenNumbers = (arr) => {
   const evenNumbersSum = arr
     .filter((num) => num % 2 === 0)
     .reduce((sum, el) => sum + el, 0);
-  console.log(evenNumbersSum);
+  return evenNumbersSum;
 };
-sumEvenNumbers([1, 2, 3]);
+console.log(sumEvenNumbers([1, 2, 3]));
