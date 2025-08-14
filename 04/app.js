666023c6be0aed24ec773729df3244414be63d5d
Natalia Kur <natalia.kur@yahoo.com>
2025-08-14 20:54:47 +0200

    Task 04: Refactor the code

--- 04/app.js
+++ 04/app.js
@@ -1,6 +1,6 @@
 const years = [1980, 1934, 2002, 2019];
 
-const passedYears = years.map(function (el, index) {
-  return 2025 - years[index];
+const passedYears = years.map(function (el) {
+  return 2025 - el;
 });
 console.log(passedYears);
